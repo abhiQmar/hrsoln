ae1e9bceb7c6bcb0a26b2a897d0a4266ba88d1b1
abhiQmar <Abhishek@Abhishek-Kumars-MacBook-Pro.local>
2015-07-15 18:33:23 +0530

    Listing all emps by index action, show action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,9 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+  def show
+    @employee = Employee.find(params[:id])
+    render inline: "<%= @employee.name %>"
+  end
 end
