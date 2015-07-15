a9549e72cff27916674ec375f4f2287e236345a6
abhiQmar <Abhishek@Abhishek-Kumars-MacBook-Pro.local>
2015-07-15 12:11:41 +0530

    Seed database values using rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+2.times do |i|
+  Employee.create(name: "Abhishek#{i}", dept: 'Backend', blood_group: 'O Positive')
+end
