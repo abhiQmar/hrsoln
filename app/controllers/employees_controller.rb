9faf4f189bd47fbff6d491285f14d07cdc2bfc95
abhiQmar <Abhishek@Abhishek-Kumars-MacBook-Pro.local>
2015-07-16 16:27:29 +0530

    Add new Employee function added

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,4 +6,14 @@ class EmployeesController < ApplicationController
     @employee = Employee.find(params[:id])
     # render inline: "<%= @employee.name %>"
   end
+  def create
+    emp = Employee.new(employee_params)
+    emp.save
+    redirect_to '/employees'
+  end
+
+private
+  def employee_params
+    params.require(:employee).permit(:name,:dept)
+  end
 end
