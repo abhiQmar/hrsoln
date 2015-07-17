a1f55eb684def35300f73f664066f25adaa67486
abhiQmar <Abhishek@Abhishek-Kumars-MacBook-Pro.local>
2015-07-17 11:50:56 +0530

    added spec for employees

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -3,7 +3,22 @@ require 'rails_helper'
 # require '/app/models/employee.rb'
 
 describe Employee do
-  # it 'is named ash' do
-  #
+  it 'should not be valid without a name' do
+    emp = Employee.new
+    emp.should_not be_valid
+  end
+  it 'raises an error when saved without a name' do
+    emp = Employee.new
+    expect {emp.save!}.to raise_error(
+      ActiveRecord::RecordInvalid
+    )
+  end
+  xit 'is named Abhi' do
+    emp = Employee.new
+    emp.name.should == 'Abhi'
+  end
+  # it 'should be in the dept it is assigned to' do
+  #   employee = Employee.new
+  #   employee.should be_in_role('assigned dept')
   # end
 end
