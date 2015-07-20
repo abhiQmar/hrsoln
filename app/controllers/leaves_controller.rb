ee58cae29aca25b912c1dcc3eca23f82283cfca5
abhiQmar <Abhishek@AKs-Mac.local>
2015-07-20 16:32:52 +0530

    adding leave to database working

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -1,4 +1,14 @@
 class LeavesController < ApplicationController
   def new
   end
+  def create
+    leave = Leave.new(leave_params)
+    leave[:employee_id] = params[:id]
+    leave.save
+    render inline: "<meta HTTP-EQUIV='REFRESH' content='2; url=/employees'>Leave has been added"
+  end
+private
+  def leave_params
+    params.require(:leave).permit(:days,:title,:description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   get '/employees/:id/leaves/new', to: 'leaves#new'
-  # You can have the root of your site routed with "root"
+  post '/employees/:id/leaves/new', to: 'leaves#create'
+   # You can have the root of your site routed with "root"
   root 'employees#index'
 
   resources :employees
