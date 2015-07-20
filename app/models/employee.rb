1e12a339aa2b28ab76cfb1ffebfee04bd9c1a87b
abhiQmar <Abhishek@AKs-Mac.local>
2015-07-20 14:57:35 +0530

    added associations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ActiveRecord::Base
+  has_many :leaves, inverse_of: :employee, dependent: :destroy
   validates :name, presence: true
   def new
   end
--- app/models/leave.rb
+++ app/models/leave.rb
@@ -1,2 +1,3 @@
 class Leave < ActiveRecord::Base
+  belongs_to :employee, inverse_of: :leaves
 end
